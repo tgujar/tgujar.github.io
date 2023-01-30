8fe342914ab20e721ef4738442128728439413a6
tgujar <tanmaygujar999@gmail.com>
2023-01-29 19:05:51 -0800

    minor fixes

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,25 @@ export default function Home({ posts }) {
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Hey, I’m Tanmay Gujar
         </h1>
-        <h2 className="prose text-gray-600 dark:text-gray-400 mb-16">
+        <h2 className="prose text-gray-600 dark:text-gray-400 mb-4">
           I’m a MS CS student at UC San Diego. You’ve found my personal slice of
-          the internet. I am interested in Distributed Systems, OS, Networks and
-          all sorts of things.
+          the internet.
+        </h2>
+        <h2 className="prose text-gray-600 dark:text-gray-400 mb-4">
+          I am interested in Distributed Systems, OS, Networks and all sorts of
+          things.
         </h2>
       </div>
 
-      <div className="flex flex-col flex-wrap items-start justify-center max-w-2xl mx-auto mb-16">
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+      <div className="flex flex-col flex-wrap max-w-2xl mx-auto mb-16">
+        <h1 className="mb-4 text-3xl flex-initial font-bold tracking-tight text-black md:text-5xl dark:text-white">
           Blog posts <Link href="/blog">→</Link>
         </h1>
         <Suspense fallback={null}>
           {!posts.length && (
-            <p className="mb-4 text-gray-600 dark:text-gray-400">
-              No posts found.
+            <p className="mb-4 text-gray-600 dark:text-gray-400 al">
+              No posts found. I am currently working on a couple projects, be
+              sure to check again!
             </p>
           )}
           {posts.map((post) => (
